caf8af0fb0973de1256ca088f0da221691046ee3
zhuzivq <141465993+zhuzivq@users.noreply.github.com>
2023-12-15 18:32:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 WORKDIR="$(pwd)"
 
 # ZyClang
-ZYCLANG_DLINK="https://github.com/ZyCromerZ/Clang/releases/download/18.0.0-20231115-release/Clang-18.0.0-20231115.tar.gz"
+ZYCLANG_DLINK="https://github.com/ZyCromerZ/Clang/releases/download/18.0.0git-20231215-release/Clang-18.0.0git-20231215.tar.gz"
 ZYCLANG_DIR="$WORKDIR/ZyClang/bin"
 
 # Kernel Source
