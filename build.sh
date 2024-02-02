35b9a6dd22990b289012428c03f1d076a93e9311
zhuzivq <141465993+zhuzivq@users.noreply.github.com>
2024-02-02 12:16:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -94,6 +94,7 @@ msg " • 🌸 $(grep 'CONFIG_LOCALVERSION=' $DEVICE_DEFCONFIG_FILE) 🌸 "
 sed -i "/CONFIG_LOCALVERSION=\"/s/.$/-KSU-$KERNELSU_VERSION\"/" $DEVICE_DEFCONFIG_FILE
 cd $KERNEL_DIR/drivers && git clone -b $RWPROCMEM_BRANCHE $RWPROCMEM_GIT
 sed -i '1i obj-y += rwProcKernelmodules/' $KERNEL_DIR/drivers/Makefile
+sed -i '1i obj-y += rwProcKernelmodule/hwBreakpointProcModule/hwBreakpointProc/' $KERNEL_DIR/drivers/Makefile
 cd $KERNEL_DIR
 
 # BUILD KERNEL
